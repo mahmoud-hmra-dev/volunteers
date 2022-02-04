69c199eb352f9c754ca085a42dff651e6e13f3e2
mahmoud-hmra-dev <92995439+mahmoud-hmra-dev@users.noreply.github.com>
2022-02-04 12:11:12 +0200

    z

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'admin/skills';
 
     /**
      * Create a new controller instance.
